06eebe7a649425d077b12cc4fe8505cd1bfe9124
Adele Kapellusch <akoutia@email.arizona.edu>
2018-02-07 16:09:14 -0700

    Adjusting Arima

--- arima.py
+++ arima.py
@@ -6,18 +6,37 @@ from pymongo import MongoClient
 from statsmodels.tsa.arima_model import ARIMA
 from sklearn.metrics import mean_squared_error
 from statsmodels.graphics.tsaplots import plot_pacf
+import datetime as dt
 
 
 def create_timeseries(manager, ticker):
+    """
+    Creates a timeseries of a chain of opening
+    and closing prices for specified stock.
+    :param manager: collection manager
+    :param ticker: string of ticker
+    :return: timeseries
+    """
     mmData = manager.find({'ticker': ticker})
     series = []
+    dates = []
     for index, row in mmData.iterrows():
         series.append(row['open'])
         series.append(row['close'])
-    return series
+        dates.append(dt.datetime.strptime(row['date']+'-09',"%Y-%m-%d-%H"))
+        dates.append(dt.datetime.strptime(row['date']+'-04',"%Y-%m-%d-%H"))
+    return series,dates
 
 
 def parameterize_arima(series, ticker):
+    """
+    Used to determine the order of the ARIMA.
+    Plots the data, autocorrelation plot,
+    partial autocorrelation plot, and residuals.
+    :param series: timeseries
+    :param ticker: string of ticker
+    :return: None
+    """
     # Plot time series
     plt.plot(range(1, len(series) + 1), series)
     plt.title(ticker)
@@ -26,15 +45,17 @@ def parameterize_arima(series, ticker):
     # Plot autocorrelation plot
     pdplot.autocorrelation_plot(series)
     plt.title(ticker + ' Autocorrelation Plot')
+    plt.savefig(ticker+'Autocor.png')
     plt.show()
 
     # Plot Partial Autocorrelation
     plot_pacf(series, lags=50)
     plt.title(ticker+' Partial Autocorrelation')
+    plt.savefig(ticker+'ParAutocor.png')
     plt.show()
 
     # Fit Model
-    model = ARIMA(series, order=(3, 1, 0))
+    model = ARIMA(series, order=(2, 1, 0))
     model_fit = model.fit(disp=0)
     print(model_fit.summary())
 
@@ -45,14 +66,22 @@ def parameterize_arima(series, ticker):
     plt.show()
     residuals.plot(kind='kde')
     plt.title(ticker + ' ARIMA residual density plot')
+    plt.savefig(ticker+'Resid.png')
     plt.show()
     print(residuals.describe())
 
 
-def fit_arima(series, p, d, q):
+def fit_arima(series, p, d, q,dates):
+    """
+    Fits the timeseries data to an ARIMA model.
+    :param series: timeseries data
+    :param p: lag order
+    :param d: differencing order
+    :param q: size of moving average window
+    :return:
+    """
     size = int(len(series) * 0.66)
     train, test = series[0:size], series[size:len(series)]
-    # history = [x for x in train]
     predictions = list()
     for t in range(len(test)):
         model = ARIMA(train, order=(p, d, q))
@@ -65,16 +94,22 @@ def fit_arima(series, p, d, q):
     error = mean_squared_error(test, predictions)
     print('Test MSE: %.3f' % error)
     # plot
-    plt.plot(test, label='Ground Truth')
-    plt.plot(predictions, color='red', label='ARIMA Predictions', alpha=0.8)
-    plt.legend()
-    plt.text(2, 2, 'Test MSE = ' + str(error))
-    plt.savefig('arima.pdf')
+    testDates = dates[size:]
+    plt.plot(dates,train,label='Training data',color='black')
+    plt.plot(testDates,test, label='Test data',color = 'blue')
+    plt.plot(testDates,predictions, color='red', label='ARIMA Predictions')
+    plt.legend(loc=4)
+    plt.text(min(dates),max(train), 'Test MSE = ' + str(error))
+    plt.title('{0} ARIMA({1},{2},{3})'.format(ticker,p,d,q))
+    plt.savefig(ticker+'Arima.pdf')
     plt.show()
 
 
 if __name__ == '__main__':
     manager = CollectionManager('5Y_technicals', MongoClient()['AlgoTradingDB'])
-    ticker = 'aapl'
-    parameterize_arima(create_timeseries(manager,ticker),ticker)
+    ticker = 'gd'
+    test = create_timeseries(manager,ticker)
 
+    series,dates = create_timeseries(manager,ticker)
+    parameterize_arima(series,ticker)
+    fit_arima(series,2,1,0,dates)
