1babbd39447a4ab44005f4de515a93b296bd2675
Adele Kapellusch <akoutia@email.arizona.edu>
2018-02-22 16:01:14 -0700

    Making arima class

--- arima.py
+++ arima.py
@@ -9,91 +9,79 @@ from statsmodels.graphics.tsaplots import plot_pacf
 from putAndGetData import create_timeseries
 
 
-def parameterize_arima(series, ticker,p,d,q):
-    """
-    Used to determine the order of the ARIMA.
-    Plots the data, autocorrelation plot,
-    partial autocorrelation plot, and residuals.
-    :param series: timeseries
-    :param ticker: string of ticker
-    :return: None
-    """
-    # Plot time series
-    plt.plot(range(1, len(series) + 1), series)
-    plt.title(ticker)
-    plt.close()
+class ArimaModel(object):
+    def __init__(self,p,d,q,ticker):
+        self.p = p
+        self.d = d
+        self.q = q
+        self.ticker = ticker
 
-    # Plot autocorrelation plot
-    pdplot.autocorrelation_plot(series)
-    plt.title(ticker + ' Autocorrelation Plot')
-    plt.savefig('plots/ARIMA/{0}Autocor.pdf'.format(ticker))
-    plt.close()
+    def plot_timeseries(self,series):
+        plt.plot(range(1, len(series) + 1), series)
+        plt.title(self.ticker)
+        plt.show()
 
-    # Plot Partial Autocorrelation
-    plot_pacf(series, lags=50)
-    plt.title(ticker+' Partial Autocorrelation')
-    plt.savefig('plots/ARIMA/{0}ParAutocor.pdf'.format(ticker))
-    plt.close()
-
-    # Fit Model
-    model = ARIMA(series, order=(p, d, q))
-    model_fit = model.fit(disp=0)
-    print(model_fit.summary())
-
-    # Plot residual errors
-    residuals = pd.DataFrame(model_fit.resid)
-    residuals.plot()
-    plt.title(ticker + ' ARIMA residuals')
-    plt.savefig('plots/ARIMA/{0}Resid_{1}{2}{3}.pdf'.format(ticker,p,d,q))
-    plt.close()
-    print(residuals.describe())
-
-
-def fit_arima(series, p, d, q,dates,window=0,plot=True):
-    """
-    Fits the timeseries data to an ARIMA model.
-    :param series: timeseries data
-    :param p: lag order
-    :param d: differencing order
-    :param q: size of moving average window
-    :return: MSE
-    """
-    size = int(len(series) * 0.66)
-    train, test = series[0:size], series[size:len(series)]
-    predictions = list()
-    w = 0
-    for t in range(len(test)+1):
-        model = ARIMA(train, order=(p, d, q))
-        model_fit = model.fit(disp=0)
-        output = model_fit.forecast()
-        yhat = output[0][0]
-        predictions.append(yhat)
-        obs = yhat
-        if w == window and t != len(test):
-            train.append(test[t])
-            w = 0
-        else:
-            train.append(obs)
-            w += 1
-    error = mean_squared_error(test, predictions[:len(test)])
-
-    # plot
-    if plot:
-        fig = plt.figure()
-        ax1 = fig.add_subplot(111)
-        ax2 = ax1
-        ax3 = ax1
-        testDates = dates[size:]
-        ax1.plot(dates[:size+1],train[:size+1],label='Training data',color='black')
-        ax2.plot(testDates,test, label='Test data',color = 'blue')
-        ax3.plot(testDates,predictions[:len(testDates)], color='red', label='ARIMA Predictions')
-        plt.legend()
-        plt.text(min(dates),max(train), 'Test MSE = ' + str(error))
-        plt.title('{0} ARIMA({1},{2},{3})'.format(ticker,p,d,q))
-        plt.savefig('plots/ARIMA/{0}Arima.pdf'.format(ticker))
+    def plot_autocorr(self,series):
+        pdplot.autocorrelation_plot(series)
+        plt.title(self.ticker + ' Autocorrelation Plot')
+        plt.savefig('plots/ARIMA/{0}Autocor.pdf'.format(self.ticker))
         plt.close()
-        print(predictions[len(testDates)])  # Next day's prediction
-    return error
+
+    def plot_partial_autocorr(self,series):
+        plot_pacf(series, lags=50)
+        plt.title(self.ticker + ' Partial Autocorrelation')
+        plt.savefig('plots/ARIMA/{0}ParAutocor.pdf'.format(self.ticker))
+        plt.close()
+
+    def plot_residuals(self,series):
+        model = ARIMA(series, order=(self.p, self.d, self.q))
+        model_fit = model.fit(disp=0)
+        print(model_fit.summary())
+
+        residuals = pd.DataFrame(model_fit.resid)
+        residuals.plot()
+        plt.title(self.ticker + ' ARIMA residuals')
+        plt.savefig('plots/ARIMA/{0}Resid_{1}{2}{3}.pdf'.format(self.ticker,self.p,self.d,self.q))
+        plt.close()
+        print(residuals.describe())
+
+    def fit_arima(self,series,dates,window=0,plot=True):
+        size = int(len(series) * 0.66)
+        train, test = series[0:size], series[size:len(series)]
+        predictions = list()
+        w = 0
+        for t in range(len(test)+1):
+            model = ARIMA(train, order=(self.p, self.d, self.q))
+            model_fit = model.fit(disp=0)
+            output = model_fit.forecast()
+            yhat = output[0][0]
+            predictions.append(yhat)
+            obs = yhat
+            if w == window and t != len(test):
+                train.append(test[t])
+                w = 0
+            else:
+                train.append(obs)
+                w += 1
+        error = mean_squared_error(test, predictions[:len(test)])
+        testDates = dates[size:]
+
+        # plot
+        if plot:
+            fig = plt.figure()
+            ax1 = fig.add_subplot(111)
+            ax2 = ax1
+            ax3 = ax1
+            ax1.plot(dates[:size+1],train[:size+1],label='Training data',color='black')
+            ax2.plot(testDates,test, label='Test data',color = 'blue')
+            ax3.plot(testDates,predictions[:len(testDates)], color='red', label='ARIMA Predictions')
+            plt.legend()
+            plt.text(min(dates),max(train), 'Test MSE = ' + str(error))
+            plt.title('{0} ARIMA({1},{2},{3})'.format(self.ticker,self.p,self.d,self.q))
+            plt.savefig('plots/ARIMA/{0}Arima.pdf'.format(self.ticker))
+            plt.close()
+        nextDaysPred = predictions[len(testDates)]
+        return error,nextDaysPred
 
 
 if __name__ == '__main__':
@@ -105,5 +93,8 @@ if __name__ == '__main__':
     ar = 1  # base predictions on last day
     i = 1  # single differencing
     ma = 0  # only autoregressive components
-    parameterize_arima(series, ticker,ar,i,ma)
-    fit_arima(series,ar,i,ma,dates)
+
+    model = ArimaModel(ar,i,ma,ticker)
+    mse,prediction = model.fit_arima(series,dates)
+
+    print('debug')
--- lowRiskStrat.py
+++ lowRiskStrat.py
@@ -3,6 +3,7 @@ from pymongo import MongoClient
 from putAndGetData import get_day_stats
 from arima import fit_arima
 
+
 class Strategy(object):
     def __init__(self,predictionFunction,manager,ticker,currentDate):
         self.predFunc = predictionFunction
@@ -10,7 +11,7 @@ class Strategy(object):
         self.ticker = ticker
         self.currentDate = currentDate
 
-    def daily_avg_price(self,close,high,low):
+    def daily_avg_price(self):
         close, high, low = get_day_stats(self.manager, self.ticker, self.currentDate)
         return (close + high + low) / 3.0
 
@@ -19,17 +20,19 @@ class Strategy(object):
         return ((P - close) / close)
 
     def arithmetic_returns(self, k, *args):
-        dailyAvgPrice = self.daily_avg_price(self.manager, self.ticker, self.currentDate)
+        dailyAvgPrice = self.daily_avg_price()
         Vi = []
         for j in range(1, k + 1):
             d = laterDate(self.currentDate, j)
             predictedPrice = self.predFunc(*args)
             Vi.append(self.daily_avg_price(self.manager, self.ticker, d))
 
+
 class LowRisk(Strategy):
-
-
-
+    def __init__(self,p,stopLoss,days):
+        self.p = p
+        self.stopLoss = stopLoss
+        self.days = days
 
 
 
@@ -44,3 +47,5 @@ if __name__ == '__main__':
     ticker = 'googl'
     date = '2017-02-02'
     strat = Strategy(fit_arima,manager,ticker,date)
+    strat.arithmetic_returns()
+    print('debug')
