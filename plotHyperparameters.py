7b41a2c7f1022c54bf60393c8bb9906c8ebd4eb0
Adele Kapellusch <akoutia@email.arizona.edu>
2018-03-08 13:09:42 -0700

    Bulk grid search plotting set up

--- plotHyperparameters.py
+++ plotHyperparameters.py
@@ -1,21 +1,25 @@
 from mongoObjects import CollectionManager,MongoClient
 from gridSearch import GridSearch
 import numpy as np
+import pandas as pd
 
-if __name__ == '__main__':
-    ps = [round(i,3) for i in np.arange(0.001, 0.033, 0.004)]  # 8 options
-    sharePers = [round(j,2) for j in np.arange(0.01, 0.46, 0.04)]  # 12 options
 
-    manager = CollectionManager('grid_search', MongoClient()['AlgoTradingDB'])
-    grid = GridSearch(manager, 'aapl',ps,sharePers)
+def plot_gridSearch_results(manager,ticker):
+    ps = [round(i, 3) for i in np.arange(0.001, 0.033, 0.004)]  # 8 options
+    sharePers = [round(j, 2) for j in np.arange(0.01, 0.46, 0.04)]  # 12 options
+    grid = GridSearch(manager, ticker, ps, sharePers)
     returns = grid.get_grid_search_data('return')
     returns = returns.pivot(index='p', columns='sharePer', values='return')
-    grid.plot_heatmap(returns,'Return',"Greens")
+    grid.plot_heatmap(returns, 'Return', "Greens")
 
     mdds = grid.get_grid_search_data('MDD')
     mdds = mdds.pivot(index='p', columns='sharePer', values='MDD')
-    grid.plot_heatmap(mdds,'Drawdown',"Purples")
+    grid.plot_heatmap(mdds, 'Drawdown', "Purples")
 
 
+if __name__ == '__main__':
+    manager = CollectionManager('grid_search', MongoClient()['AlgoTradingDB'])
+    stocks = [symbol.lower() for symbol in list(pd.read_csv('stocks.csv')['Symbol'])]
 
-
+    for stock in stocks:
+        plot_gridSearch_results(manager,stock)
