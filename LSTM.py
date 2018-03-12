60be74d35c253276b81c04f0ff9290232a524b1d
Adele Kapellusch <akoutia@email.arizona.edu>
2018-03-12 16:25:11 -0700

    Fixed NN

--- LSTM.py
+++ LSTM.py
@@ -14,6 +14,8 @@ import matplotlib.pyplot as plt
 class NN(object):
     def __transform_data(self,data:list):
         trainIndex = int(0.75 * len(data))
+        self.rawTrain = data[:trainIndex]
+        self.rawTest = data[trainIndex:]
         diff_values = self.difference(data)  # Make timeseries stationary
         train = self.timeseries_to_supervised(diff_values[:trainIndex+1])
         test = self.timeseries_to_supervised(diff_values[trainIndex:])
@@ -22,6 +24,8 @@ class NN(object):
 
     def __init__(self,data:list):
         self.raw_data = data
+        self.rawTrain = None
+        self.rawTest = None
         self.scaler = MinMaxScaler(feature_range=(-1, 1))
         self.train, self.test = self.__transform_data(data)
         self.model = None
@@ -69,7 +73,7 @@ class NN(object):
         inverted = self.scaler.inverse_transform(array)
         return inverted[0, -1]
 
-    def fit(self,batch_size, nb_epoch, neurons):
+    def fit_lstm(self,batch_size, nb_epoch, neurons):
         X, y = self.train[:, 0:-1], self.train[:, -1]
         X = X.reshape(X.shape[0], 1, X.shape[1])
         model = Sequential()
@@ -80,34 +84,16 @@ class NN(object):
             model.fit(X, y, epochs=1, batch_size=batch_size, verbose=0, shuffle=False)
             model.reset_states()
         self.model = model
+        train_reshaped = network.train[:, 0].reshape(len(network.train), 1, 1)
+        network.model.predict(train_reshaped, batch_size=1)
 
-    def forecast(self, batch_size, X):
+    def fit(self, X):
         # A one step forecast
         X = X.reshape(1, 1, len(X))
-        yhat = self.model.predict(X, batch_size=batch_size)
-        return yhat[0, 0]
-
-    def predict(self):
-        train_reshaped = self.train[:, 0].reshape(len(self.train), 1, 1)
-        self.model.predict(train_reshaped, batch_size=1)
-
-        # walk-forward validation on the test data
-        predictions = list()
-        exp = list()
-        for i in range(len(self.test)):
-            # make one-step forecast
-            X, y = self.test[i, 0:-1], self.test[i, -1]
-            yhat = self.forecast(1, X)
-            # invert scaling
-            yhat = self.invert_scale(X, yhat)
-            # invert differencing
-            yhat = self.invert(self.raw_data, yhat, len(self.test) + 1 - i)
-            # store forecast
-            predictions.append(yhat)
-            expected = self.raw_data[len(self.train) + i + 1]
-            exp.append(expected)
-            # print('Predicted={0}, Expected={1}'.format(yhat, expected))
-        return predictions,exp
+        yhat = self.model.predict(X, batch_size=1)[0,0]
+        yhat = self.invert_scale(X, yhat)
+        yhat = self.invert(self.raw_data, yhat, len(self.test) + 1 - 0)
+        return yhat
 
 
 
@@ -118,12 +104,9 @@ if __name__ == '__main__':
     data = create_timeseries(manager,'aapl')[0]
 
     network = NN(data)
-    network.fit(1, 10, 4)
-    predictions,expected = network.predict()
+    network.fit_lstm(1, 10, 4)
+    prediction = network.fit(network.test[0, 0:-1])
+    actual = network.rawTest[0]
 
-    rmse = sqrt(mean_squared_error(expected, predictions))
-    plt.plot(predictions)
-    plt.plot(expected)
-    plt.show()
-
-    print('Test RMSE: %.3f' % rmse)
+    print(prediction)
+    print(actual)
