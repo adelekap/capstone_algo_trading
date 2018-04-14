28b627d25dc82b4e3aa6db6aaeba6244676488ec
Adele Kapellusch <akoutia@email.arizona.edu>
2018-04-14 13:10:59 -0700

    Average Sector Performance

--- sectorAnalysis.py
+++ sectorAnalysis.py
@@ -1,9 +1,10 @@
 import pandas as pd
 from mongoObjects import CollectionManager
 from putAndGetData import avg_price_timeseries
+import numpy as np
 
 
-def create_sec_dic():
+def create_sec_dic(stocks):
     sectorToStocks = {}
     for index,stock in stocks.iterrows():
         currentSector = stock['Sector']
@@ -23,14 +24,33 @@ def get_data_by_sector(stocks,sector):
         data[stock] = stocksData
     data.to_csv('sectorAnalysis/{0}.csv'.format(sector))
 
+def get_data_to_files():
+
+    sectorToStocks = create_sec_dic(stocks)
+
+    for sector in sectors:
+        get_data_by_sector(sectorToStocks[sector], sector)
+
+def avg_data_files():
+    avg = pd.DataFrame()
+    for sector in sectors:
+        data = pd.read_csv('sectorAnalysis/{0}.csv'.format(sector))
+        avgs = []
+        for i,day in data.iterrows():
+            avgs.append(np.mean(day))
+        avg[sector] = avgs
+    avg.to_csv('sectorAnalysis/AveragePerformance.csv')
 
 if __name__ == '__main__':
     manager = CollectionManager('5Y_technicals', 'AlgoTradingDB')
     stocks = pd.read_csv('stocks.csv')
     sectors = stocks['Sector'].unique()
+    # get_data_to_files()
 
-    sectorToStocks = create_sec_dic()
+    avg_data_files()
 
-    for sector in sectors:
-        get_data_by_sector(sectorToStocks[sector],sector)
+
+
+
+
 
