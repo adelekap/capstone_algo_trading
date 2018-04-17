4f1581586662392b3ea4966540b5bc8578a3ac4a
Adele Kapellusch <akoutia@email.arizona.edu>
2018-04-17 01:14:54 -0700

    Trying to make training data for stock suggester

--- addFundamentals.py
+++ addFundamentals.py
@@ -5,6 +5,8 @@ from datetime import date
 import datetime
 import datedelta
 import numpy as np
+import calendar
+from pandas.tseries.offsets import BDay
 
 quarters = {1: "Q4", 2: "Q4", 3: "Q4",
             4: "Q1", 5: "Q1", 6: "Q1",
@@ -18,6 +20,47 @@ def get_quarter(dateObject: date) -> str:
     previous = quarters[month - 3 if month >= 4 else (month - 3) + 12] + " " + \
                str(dateObject.year if quarters[month] != "Q4" else dateObject.year - 1)
     return current, previous
+
+
+def next_quarter(current: str):
+    year = int(current.split(' ')[1])
+    q = int(current[1])
+    if q < 4:
+        q += 1
+    else:
+        year += 1
+        q = 1
+    return f'Q{q} {year}'
+
+
+def get_next_business_day(current_date: date):
+    day_of_week = current_date.weekday()
+    next_business_day = current_date
+
+    if day_of_week > 4:
+        next_business_day = current_date + (datedelta.DAY * (7 - day_of_week))
+
+    return next_business_day
+
+
+def calculate_performance(ticker, date1: date, date2: date):
+    ticker = ticker.lower()
+    manager = CollectionManager('5Y_technicals', 'AlgoTradingDB')
+
+    date1 = get_next_business_day(date1)
+    date2 = get_next_business_day(date2)
+
+    print(ticker,date1,date2)
+    price1 = manager.find({'ticker': ticker, 'date': str(get_next_business_day(date1))})
+    if price1.shape[0] == 0:
+        price1 = manager.find({'ticker':ticker,'date':str(get_next_business_day(date1) + datetime.timedelta(days=1))})
+    price1 = price1['vwap'][0]
+    price2 = manager.find({'ticker': ticker, 'date': str(get_next_business_day(date2))})
+    if price2.shape[0] == 0:
+        price2 = manager.find({'ticker':ticker,'date':str(get_next_business_day(date2)+ datetime.timedelta(days=1))})
+    price2 = price2['vwap'][0]
+    manager.close()
+    return (price2 - price1) / price2
 
 
 def months_apart(earlier_date: date, later_date: date) -> int:
@@ -40,7 +83,7 @@ def get_all_past_quarters(today_date: date):  # feb 5th 2013 - today's date
     return available_quarters
 
 
-def get_all_future_quarters(today_date: date): # feb 5th 2018 - today's date
+def get_all_future_quarters(today_date: date):  # feb 5th 2018 - today's date
     future_date = date(2018, 2, 5)
 
     if today_date > future_date:
@@ -54,6 +97,7 @@ def get_all_future_quarters(today_date: date): # feb 5th 2018 - today's date
         available_quarters.append(get_quarter(future_date)[0])
 
     return available_quarters
+
 
 def add_fundamentals_to_db():
     """
@@ -77,10 +121,15 @@ def get_all_fundamentals(tickers: list, quarters: list):
     for quarter in quarters:
         quarterly = []
         for ticker in tickers:
-            data = manager.find({'ticker': ticker.upper(), 'quarter': quarter}).iloc[:, :-4]
+            data = manager.find({'ticker': ticker.upper(), 'quarter': quarter}).iloc[:, :-2]
             if len(data) == 0:
                 continue
-            
+            announcementDate = data.iloc[:, -1][0].date()
+            data = data.iloc[:, :-2]
+            nextAnnouncementDate = manager.find({'ticker': ticker.upper(),
+                                                 'quarter': next_quarter(quarter)})['date'][0].date()
+            performance = calculate_performance(ticker, announcementDate, nextAnnouncementDate)
+            performances.append(performance)
             quarterly.append(data.values[0])
         allFundamentals.append(quarterly)
     return np.array(allFundamentals)
@@ -111,4 +160,5 @@ def get_fundamental_data(tickers: list, dateString):
 
 
 if __name__ == '__main__':
-    get_fundamental_data(['aapl', 'bac'], '2018-01-01')
+    # get_fundamental_data(['aapl', 'bac'], '2018-01-01')
+    print(get_next_business_day(date(2013, 3, 31)))
