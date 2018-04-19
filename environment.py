b26225ce14d52cd9a70d04f7b05bc0afb4772f6f
Adele Kapellusch <akoutia@email.arizona.edu>
2018-04-19 15:25:09 -0700

    Updated environment

--- environment.py
+++ environment.py
@@ -208,7 +208,7 @@ if __name__ == '__main__':
     """Arguments"""
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', choices=['Arima', 'LSTM', 'SVM'], metavar='M',
-                        help='predictive model to use', default='Arima', required=False)  # Todo: add other choices
+                        help='predictive model to use', default='Arima', required=False) 
     parser.add_argument('--startDate', help='start date YYYY-MM-DD', default='2017-09-05', required=False, type=str)
     parser.add_argument('--startingCapital', help='amount of money to start with', default=5000.00, type=float,
                         required=False)
